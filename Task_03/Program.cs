172a50cd3355bdcab9ffede462b07914ecb3d367
Tatiana Zhandarova <vojjtikunova@gmail.com>
2024-01-06 22:08:11 +0300

    третья задача

---
+++ Task_03/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте произвольный массив. Выведете его элементы, 
+//начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] array = NewMassive(5);
+PrintArray(array);
+Console.WriteLine();
+backArray(array,array.Length-1);
+
+
+int [] NewMassive(int j){
+    int[] count = new int [j];
+    for(int i = 0; i<count.Length; i++) count[i] = new Random().Next(1,100);
+    return count;
+}
+
+void PrintArray(int[] col){
+    int count = col.Length;
+    int position = 0;
+    while(position<count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+void backArray(int [] num, int i){
+    if(i>=0){
+        Console.Write(num[i] + " ");
+        backArray(num, i-1);
+    }
+}
